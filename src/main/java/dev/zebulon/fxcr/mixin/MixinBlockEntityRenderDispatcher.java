166f7e05debbf92ae709b65065b0d723fd09c712
Jordin <git@jord.in>
2020-11-10 16:17:54 -0400

    change target for dispatcher optimisation

--- src/main/java/dev/zebulon/fxcr/mixin/MixinBlockEntityRenderDispatcher.java
+++ src/main/java/dev/zebulon/fxcr/mixin/MixinBlockEntityRenderDispatcher.java
@@ -1,23 +1,22 @@
 package dev.zebulon.fxcr.mixin;
 
+import net.minecraft.client.render.block.entity.BlockEntityRenderer;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
-import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.block.entity.BlockEntityRenderDispatcher;
-import net.minecraft.client.util.math.MatrixStack;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(BlockEntityRenderDispatcher.class)
 public class MixinBlockEntityRenderDispatcher {
-    @Inject(method = "renderEntity(Lnet/minecraft/block/entity/BlockEntity;Lnet/minecraft/client/util/math/MatrixStack;Lnet/minecraft/client/render/VertexConsumerProvider;II)Z", at = @At("HEAD"), cancellable = true)
-    public <E extends BlockEntity> void renderEntity(E entity, MatrixStack matrix,
-            VertexConsumerProvider vertexConsumerProvider, int light, int overlay, CallbackInfoReturnable<Boolean> callbackInfo) {
-        if (entity.hasWorld() && entity.getType() == BlockEntityType.CHEST) {
-            callbackInfo.setReturnValue(true);
+    @Inject(method = "get(Lnet/minecraft/block/entity/BlockEntity;)Lnet/minecraft/client/render/block/entity/BlockEntityRenderer;", at = @At("HEAD"), cancellable = true)
+    public <E extends BlockEntity> void renderEntity(E blockEntity, CallbackInfoReturnable<@Nullable BlockEntityRenderer<E>> cir) {
+        if (blockEntity.hasWorld() && blockEntity.getType() == BlockEntityType.CHEST) {
+            cir.setReturnValue(null);
         }
     }
 }
