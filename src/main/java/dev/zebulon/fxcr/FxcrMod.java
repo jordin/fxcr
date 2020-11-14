805eec3c8d6c7080655ceeff1846ad9fc9a973e7
vlakreeh <zeb@zebulon.dev>
2020-11-14 09:56:23 -0500

    Add game option

--- src/main/java/dev/zebulon/fxcr/FxcrMod.java
+++ src/main/java/dev/zebulon/fxcr/FxcrMod.java
@@ -5,11 +5,28 @@ import dev.zebulon.fxcr.block.FastTrappedChestBlock;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Material;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.options.CyclingOption;
+import net.minecraft.client.options.Option;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 public class FxcrMod implements ModInitializer {
-	public static final boolean ENABLED = System.getProperty("FXCR.DISABLED") == null;
+	public static boolean ENABLED = System.getProperty("FXCR.DISABLED") == null;
+
+	// Eclipse shows resource leak warnings on all calls for getInstance, ugh
+	@SuppressWarnings("all")
+	public static final Option FXCR_ENABLED_OPTION = CyclingOption.method_32522("fxcr.enabled", (gameOptions) -> {
+		return ENABLED;
+	 }, (gameOptions, option, boolean_) -> {
+		ENABLED = !ENABLED;
+
+		if (MinecraftClient.getInstance().world == null) {
+			return;
+		}
+
+		MinecraftClient.getInstance().worldRenderer.reload();
+	 });
 
 	public static final FastChestBlock FAST_CHEST_BLOCK = new FastChestBlock(FabricBlockSettings.of(Material.WOOD).hardness(4.0f));
 	public static final FastTrappedChestBlock FAST_TRAPPED_CHEST_BLOCK = new FastTrappedChestBlock(FabricBlockSettings.of(Material.WOOD).hardness(4.0f));
---
+++ src/main/java/dev/zebulon/fxcr/mixin/MixinVideoOptionsScreen.java
@@ -0,0 +1,26 @@
+package dev.zebulon.fxcr.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+
+import dev.zebulon.fxcr.FxcrMod;
+import net.minecraft.client.gui.screen.VideoOptionsScreen;
+import net.minecraft.client.options.Option;
+
+@Mixin(VideoOptionsScreen.class)
+public class MixinVideoOptionsScreen {
+
+    @Shadow
+    public static Option[] OPTIONS;
+
+    static {
+        Option[] oldOptions = OPTIONS;
+        OPTIONS = new Option[oldOptions.length + 1];
+
+        for (int i = 0; i < oldOptions.length; i++) {
+            OPTIONS[i] = oldOptions[i];
+        }
+
+        OPTIONS[OPTIONS.length - 1] = FxcrMod.FXCR_ENABLED_OPTION;
+    }
+}
