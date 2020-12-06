8f10791e0256ff841ff4add14d8c56d530d23de0
Jordin <git@jord.in>
2020-12-06 13:42:16 -0400

    Fix config loading

--- src/main/java/dev/zebulon/fxcr/mixin/MixinGameOptions.java
+++ src/main/java/dev/zebulon/fxcr/mixin/MixinGameOptions.java
@@ -3,6 +3,7 @@ package dev.zebulon.fxcr.mixin;
 import java.io.PrintWriter;
 
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
@@ -12,6 +13,11 @@ import net.minecraft.nbt.CompoundTag;
 
 @Mixin(GameOptions.class)
 public abstract class MixinGameOptions {
+    @Shadow
+    private static boolean isTrue(String string) {
+        throw new IllegalStateException("Mixin shadow method body invoked.");
+    }
+
     @Redirect(method = "write()V", at = @At(value = "INVOKE", target = "Ljava/io/PrintWriter;println(Ljava/lang/String;)V", ordinal = 0))
     private void onWritePrintLn(PrintWriter printWriter, String x) {
         printWriter.println("fxcrEnabled:" + FxcrMod.enabled);
@@ -20,7 +26,7 @@ public abstract class MixinGameOptions {
 
     @Redirect(method = "load()V", at = @At(value = "INVOKE", target = "Lnet/minecraft/nbt/CompoundTag;getString(Ljava/lang/String;)Ljava/lang/String;", ordinal = 1))
     private String onLoadGetString(CompoundTag compoundTag, String key) {
-        FxcrMod.enabled = compoundTag.getBoolean("fxcrEnabled");
+        FxcrMod.enabled = isTrue(compoundTag.getString("fxcrEnabled"));
 
         return compoundTag.getString(key);
     }
